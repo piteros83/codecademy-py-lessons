16fc1172a80384592cb028658e0b474a9bc8d4f4
Piotr Schuetz <piotr.schuetz@gmail.com>
2016-05-30 13:09:54 +0200

    lucky numbers

--- python.py
+++ python.py
@@ -494,4 +494,41 @@ while num < 11:  # Fill in the condition
     print (num ** 2)
     # Increment num (make sure to do this!)
     num += 1
-		
+
+#================================================================
+#lucky numbers
+
+import random
+
+print "Lucky Numbers! 3 numbers will be generated."
+print "If one of them is a '5', you lose!"
+
+count = 0
+while count < 3:
+    num = random.randint(1, 6)
+    print num
+    if num == 5:
+        print "Sorry, you lose!"
+        break
+    count += 1
+else:
+    print "You win!"	
+
+#================================================================	
+#lucky numbers the second
+
+from random import randint
+
+# Generates a number from 1 through 10 inclusive
+random_number = randint(1, 10)
+
+guesses_left = 3
+# Start your game!
+while guesses_left > 0:
+    guess = int(raw_input("Your guess: "))
+    if guess == random_number:
+        print "You win!"
+        break
+    guesses_left -= 1
+else:
+    print "You lose!"	
